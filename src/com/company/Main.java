7c4cdaf60c23a5fc91efd553dc66ac407f1bc48f
khaliqyar18 <khaliqyar@itt-net.com>
2021-01-26 19:55:57 -0500

    Solar System Ready

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.Sun =1;
+        FactorX.Moons = 2;
+        FactorX.planets = 3;
 
 
     }
